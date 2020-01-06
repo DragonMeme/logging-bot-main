9c91fda38a4f7cf22be8f3e8f55bb5bf5592b6f6
DragonMeme <48312022+DragonMeme@users.noreply.github.com>
2020-01-06 15:32:58 +1300

    Add commands.js
    Modularised commands in to functions and allow assigning environment variables.

---
+++ commands.js
@@ -0,0 +1,48 @@
+exports.processCommand = function(msg, prefix, client){
+    let list_variables = msg.content.toLowerCase().slice(prefix.length).split(' ');
+
+    if(String(msg.author.id) === process.env.AUTHOR_ID){
+        // These commands only apply to bot author
+        switch(list_variables[0]){
+            case "shutdown":
+                shutdown(msg, client);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    //Any user can use these commands.
+    switch(list_variables[0]){
+        case "ping":
+            ping(msg);
+            break;
+        default:
+            break;
+    }
+    
+};
+
+function ping(msg){
+    // Obtain time the user sent the message.
+    var timeSent = msg.createdTimestamp;
+    msg.channel.send('Pong!').then(
+        // Attempt to edit the message by adding the time.
+        sent => {
+
+            // Otain time stamp between user message and bot message.
+            var timePing = sent.createdTimestamp;
+            var ping = timePing - timeSent;
+
+            // Add ping message.
+            sent.edit('Pong! `' + String(ping) + 'ms`');
+        }
+    );
+}
+
+function shutdown(msg, client){
+    // Send a message before shutting down.
+    msg.channel.send("Shutting down!").then(
+        client.destroy()
+    );
+}
--- main.js
+++ main.js
@@ -1,4 +1,7 @@
+const Command = require("./commands");
 const Discord = require("discord.js");
+
+require("dotenv").config();
 
 const client = new Discord.Client();
 var prefix = ",";
@@ -15,29 +18,8 @@ client.on('message', msg => {
     // Check that the right prefix is used.
     if(!msg.content.startsWith(prefix)) return;
 
-    let list_variables = msg.content.toLowerCase().slice(prefix.length).split(' ');
-
-    // Send a ping message.
-    if(list_variables[0] === 'ping'){
-
-        // Obtain time the user sent the message.
-        timeSent = msg.createdTimestamp;
-        msg.channel.send('Pong!').then(
-            // Attempt to edit the message by adding the time.
-            sent => {
-
-                // Otain time stamp between user message and bot message.
-                var timePing = sent.createdTimestamp;
-                var ping = timePing - timeSent;
-                ping = String(ping);
-
-                // Add ping message.
-                sent.edit('Pong! `' + ping + 'ms`');
-            }
-        );
-        
-    }
+    Command.processCommand(msg, prefix, client);
 });
 
 
-client.login(BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
